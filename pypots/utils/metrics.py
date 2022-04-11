a9ed2bdf0c69277e52a546f7734eb5518d49329b
WenjieDu <wenjay.du@gmail.com>
2022-04-11 21:57:36 +0800

    feat: make error cal functions more flexible;

--- pypots/utils/metrics.py
+++ pypots/utils/metrics.py
@@ -5,32 +5,37 @@ Utilities for evaluation metrics
 # License: MIT
 
 import torch
+import numpy as np
 
 
 def cal_mae(inputs, target, mask=None):
     """ calculate Mean Absolute Error"""
+    lib = np if isinstance(inputs, np.ndarray) else torch
     if mask is not None:
-        return torch.sum(torch.abs(inputs - target) * mask) / (torch.sum(mask) + 1e-9)
+        return lib.sum(lib.abs(inputs - target) * mask) / (lib.sum(mask) + 1e-9)
     else:
-        return torch.mean(torch.abs(inputs - target))
+        return lib.mean(lib.abs(inputs - target))
 
 
 def cal_mse(inputs, target, mask=None):
     """ calculate Mean Square Error"""
+    lib = np if isinstance(inputs, np.ndarray) else torch
     if mask is not None:
-        return torch.sum(torch.square(inputs - target) * mask) / (torch.sum(mask) + 1e-9)
+        return lib.sum(lib.square(inputs - target) * mask) / (lib.sum(mask) + 1e-9)
     else:
-        return torch.mean(torch.square(inputs - target))
+        return lib.mean(lib.square(inputs - target))
 
 
 def cal_rmse(inputs, target, mask=None):
     """ calculate Root Mean Square Error"""
-    return torch.sqrt(cal_mse(inputs, target, mask))
+    lib = np if isinstance(inputs, np.ndarray) else torch
+    return lib.sqrt(cal_mse(inputs, target, mask))
 
 
 def cal_mre(inputs, target, mask=None):
     """ calculate Mean Relative Error"""
+    lib = np if isinstance(inputs, np.ndarray) else torch
     if mask is not None:
-        return torch.sum(torch.abs(inputs - target) * mask) / (torch.sum(torch.abs(target * mask)) + 1e-9)
+        return lib.sum(lib.abs(inputs - target) * mask) / (lib.sum(lib.abs(target * mask)) + 1e-9)
     else:
-        return torch.mean(torch.abs(inputs - target)) / (torch.sum(torch.abs(target)) + 1e-9)
+        return lib.mean(lib.abs(inputs - target)) / (lib.sum(lib.abs(target)) + 1e-9)
