32f51f2985d48abba8ea2feb33602559e6f5b581
WenjieDu <wenjay.du@gmail.com>
2022-04-11 15:44:06 +0800

    feat: add Corrputor;

--- pypots/datasets/base.py
+++ pypots/datasets/base.py
@@ -42,51 +42,133 @@ def generate_random_walk(n_samples=1000, n_steps=24, n_features=10, mu=0., std=1
     return ts_samples
 
 
-def random_mask(X, artificial_missing_rate, nan=0):
-    """ Randomly mask out some observed values to create artificially-missing values.
-    This step is for training model on the masked imputation task (MIT).
-    Please refer to :cite:`du2022SAITS` for more details about MIT.
+class Corruptor:
+    """ Corrupt data by adding missing values to it with optional missing patterns (MCAR,MAR,MNAR).
 
     Parameters
     ----------
-    X : array,
-        Input feature vector. If X has any missing values, they should be numpy.nan.
-    artificial_missing_rate : float in (0,1),
-        Rate of observed values which will be artificially masked as missing.
-    nan : int/float, optional,
-        Value to be used to fill NaN values.
+    data : array,
+        Time series data.
 
-    Returns
-    -------
-    X_intact : array,
-        Original X with missing values (nan) filled with given parameter `nan`, with observed values intact.
-        X_intact is for loss calculation in the masked imputation task.
-    X : array,
-        Original X with artificial missing values. X is for model input.
-        Both originally-missing and artificially-missing values are filled with given parameter `nan`.
-    missing_mask : array,
-        The mask indicates all missing values in X.
-    indicating_mask : array,
-        The mask indicates the artificially-missing values in X, namely missing parts different from X_intact.
+    rate : float in (0,1),
+        Artificially missing rate, rate of observed values which will be artificially masked as missing.
+
+        Note that,
+        `rate` = (number of artificially missing values) / np.sum(~np.isnan(self.data)),
+        not (number of artificially missing values) / np.product(self.data.shape),
+        considering that the given data may already contain missing values,
+        the latter way may be confusing because if the original missing rate >= `rate`,
+        Corruptor will do nothing, i.e. it won't play the role it has to be.
+
+    Attributes
+    ----------
+    original_shape : tuple,
+        The original shape of `data`.
     """
-    original_shape = X.shape
-    X = X.reshape(-1)
-    # select random indices for artificial mask
-    indices = np.where(~np.isnan(X))[0].tolist()  # get the indices of observed values
-    indices = np.random.choice(indices, int(len(indices) * artificial_missing_rate), replace=False)
-    # create artificially-missing values by selected indices
-    X_intact = np.copy(X)  # keep a copy of originally observed values in X_intact
-    X[indices] = np.nan  # mask values selected by indices
-    indicating_mask = ((~np.isnan(X)) ^ (~np.isnan(X))).astype(np.float32)
-    missing_mask = (~np.isnan(X)).astype(np.float32)
-    X = np.nan_to_num(X, nan=nan)
-    X_intact = np.nan_to_num(X_intact, nan=nan)
-    # reshape into time-series data
-    X_intact = X_intact.reshape(original_shape)
-    X = X.reshape(original_shape)
-    missing_mask = missing_mask.reshape(original_shape)
-    indicating_mask = indicating_mask.reshape(original_shape)
-    return X_intact, X, missing_mask, indicating_mask
+
+    def __init__(self, data, rate):
+        self.data = data
+        self.original_shape = data.shape
+        self.rate = rate
+
+    def originally_missing_rate(self):
+        """ Calculate the originally missing rate of the raw data.
+
+        Returns
+        -------
+        originally_missing_rate, float,
+            The originally missing rate of the raw data.
+        """
+        originally_missing_rate = np.sum(np.isnan(self.data)) / np.product(self.data.shape)
+        return originally_missing_rate
+
+    @staticmethod
+    def fill_nan_with_mask(data, mask):
+        """ Fill missing values in `data` with nan according to mask.
+
+        Parameters
+        ----------
+        data : array,
+            Data vector having missing values filled with numbers (i.e. not nan).
+
+        mask : array,
+            Mask vector contains binary values indicating which values are missing in `data`.
+
+        Returns
+        -------
+        array,
+            Data vector having missing values placed with np.nan.
+        """
+        assert data.shape == mask.shape, f'Shapes of data and mask must match, ' \
+                                         f'but data.shape={data.shape}, mask.shape={mask.shape}'
+        mask = bool(mask)
+        data[mask] = np.nan
+        return data
+
+    def mcar(self, nan=0):
+        """ Create completely random missing values (MCAR case).
+
+        Parameters
+        ----------
+        nan : int/float, optional,
+            Value to be used to fill NaN values.
+
+        Returns
+        -------
+        X_intact : array,
+            Original data with missing values (nan) filled with given parameter `nan`, with observed values intact.
+            X_intact is for loss calculation in the masked imputation task.
+        X : array,
+            Original X with artificial missing values. X is for model input.
+            Both originally-missing and artificially-missing values are filled with given parameter `nan`.
+        missing_mask : array,
+            The mask indicates all missing values in X.
+        indicating_mask : array,
+            The mask indicates the artificially-missing values in X, namely missing parts different from X_intact.
+        """
+        X = self.data.flatten()
+        # select random indices for artificial mask
+        indices = np.where(~np.isnan(X))[0].tolist()  # get the indices of observed values
+        indices = np.random.choice(indices, int(len(indices) * self.rate), replace=False)
+        # create artificially-missing values by selected indices
+        X[indices] = np.nan  # mask values selected by indices
+        indicating_mask = ((~np.isnan(X)) ^ (~np.isnan(X))).astype(np.float32)
+        missing_mask = (~np.isnan(X)).astype(np.float32)
+        X = np.nan_to_num(X, nan=nan)
+        X_intact = np.nan_to_num(self.data, nan=nan)
+        # reshape into time-series data
+        X = X.reshape(self.original_shape)
+        missing_mask = missing_mask.reshape(self.original_shape)
+        indicating_mask = indicating_mask.reshape(self.original_shape)
+        return X_intact, X, missing_mask, indicating_mask
+
+    def mar(self, nan=0):
+        """ Create random missing values (MAR case).
+
+        Parameters
+        ----------
+        nan : int/float, optional,
+            Value to be used to fill NaN values.
+
+        Returns
+        -------
+
+        """
+        pass
+
+    def mnar(self, nan=0):
+        """ Create not-random missing values (MNAR case).
+
+        Parameters
+        ----------
+        nan : int/float, optional,
+            Value to be used to fill NaN values.
+
+        Returns
+        -------
+
+        """
+        pass
 
 
 class BaseDataset(Dataset):
