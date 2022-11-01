fa08b17c455d1fece07973792a9c3a7171840974
Wenjie Du <wenjay.du@gmail.com>
2022-11-01 11:53:08 +0800

    feat: add options --show_coverage and -k to pypots-cli.dev;

--- pypots/utils/commands/dev.py
+++ pypots/utils/commands/dev.py
@@ -14,6 +14,8 @@ from pypots.utils.commands import BaseCommand
 def dev_command_factory(args: Namespace):
     return DevCommand(
         args.run_tests,
+        args.k,
+        args.show_coverage,
         args.lint_code,
     )
 
@@ -29,6 +31,27 @@ class DevCommand(BaseCommand):
             help="run all test cases",
         )
         train_parser.add_argument(
+            "--show_coverage",
+            dest="show_coverage",
+            action="store_true",
+            help="show the code coverage report after running tests",
+        )
+        train_parser.add_argument(
+            "-k",
+            type=str,
+            default="",
+            required=False,
+            help="the -k option of pytest. Description of -k option in pytest: "
+            "only run tests which match the given substring expression. An expression is a python evaluatable "
+            "expression where all names are substring-matched against test names and their parent classes. "
+            "Example: -k 'test_method or test_other' matches all test functions and classes whose name contains "
+            "'test_method' or 'test_other', while -k 'not test_method' matches those that don't contain "
+            "'test_method' in their names. -k 'not test_method and not test_other' will eliminate the matches. "
+            "Additionally keywords are matched to classes and functions containing extra names in their "
+            "'extra_keyword_matches' set, as well as functions which have names assigned directly to them. The "
+            "matching is case-insensitive.",
+        )
+        train_parser.add_argument(
             "--lint_code",
             dest="lint_code",
             action="store_true",
@@ -39,9 +62,13 @@ class DevCommand(BaseCommand):
     def __init__(
         self,
         run_tests: bool,
+        k: str,
+        show_coverage: bool,
         lint_code: bool,
     ):
         self._run_tests = run_tests
+        self._k = k
+        self._show_coverage = show_coverage
         self._lint_code = lint_code
 
     def run(self):
@@ -49,8 +76,23 @@ class DevCommand(BaseCommand):
 
         if self._run_tests:
             try:
-                os.system("pytest")
+                pytest_command = f"pytest -k {self._k}" if self._k else "pytest"
+                command_to_run_test = (
+                    f"coverage run -m {pytest_command}"
+                    if self._show_coverage
+                    else pytest_command
+                )
+
+                os.system(command_to_run_test)
+                if self._show_coverage:
+                    os.system("coverage report -m")
+                    os.system("rm -rf .coverage")
+                else:
+                    print(
+                        "Omit the code coverage report. Enable it by using --show_coverage if in need."
+                    )
                 os.system("rm -rf .pytest_cache")
+
             except Exception as e:
                 raise RuntimeError(e)
         elif self._lint_code:
