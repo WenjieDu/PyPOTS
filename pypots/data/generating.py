922bbfb3d1984565defa3ddf895f8989df73da52
Wenjie Du <wenjay.du@gmail.com>
2023-04-25 16:36:51 +0800

    fix: the disordered labels in the returned data;

--- pypots/data/generating.py
+++ pypots/data/generating.py
@@ -277,18 +277,22 @@ def gene_physionet2012(artificially_missing: bool = True):
         Whether to artificially mask out 10% observed values and hold out for imputation performance evaluation.
     """
     # generate samples
-    df = load_specific_dataset("physionet_2012")
-    X = df["X"]
-    y = df["y"]
+    dataset = load_specific_dataset("physionet_2012")
+    X = dataset["X"]
+    y = dataset["y"]
     all_recordID = X["RecordID"].unique()
     train_set_ids, test_set_ids = train_test_split(all_recordID, test_size=0.2)
     train_set_ids, val_set_ids = train_test_split(train_set_ids, test_size=0.2)
-    train_set = X[X["RecordID"].isin(train_set_ids)]
-    val_set = X[X["RecordID"].isin(val_set_ids)]
-    test_set = X[X["RecordID"].isin(test_set_ids)]
-    train_set = train_set.drop("RecordID", axis=1)
-    val_set = val_set.drop("RecordID", axis=1)
-    test_set = test_set.drop("RecordID", axis=1)
+    train_set_ids.sort()
+    val_set_ids.sort()
+    test_set_ids.sort()
+    train_set = X[X["RecordID"].isin(train_set_ids)].sort_values(["RecordID", "Time"])
+    val_set = X[X["RecordID"].isin(val_set_ids)].sort_values(["RecordID", "Time"])
+    test_set = X[X["RecordID"].isin(test_set_ids)].sort_values(["RecordID", "Time"])
+
+    train_set = train_set.drop(["RecordID", "Time"], axis=1)
+    val_set = val_set.drop(["RecordID", "Time"], axis=1)
+    test_set = test_set.drop(["RecordID", "Time"], axis=1)
     train_X, val_X, test_X = (
         train_set.to_numpy(),
         val_set.to_numpy(),
@@ -306,9 +310,9 @@ def gene_physionet2012(artificially_missing: bool = True):
     val_X = val_X.reshape(len(val_set_ids), 48, -1)
     test_X = test_X.reshape(len(test_set_ids), 48, -1)
 
-    train_y = y[y.index.isin(train_set_ids)]
-    val_y = y[y.index.isin(val_set_ids)]
-    test_y = y[y.index.isin(test_set_ids)]
+    train_y = y[y.index.isin(train_set_ids)].sort_index()
+    val_y = y[y.index.isin(val_set_ids)].sort_index()
+    test_y = y[y.index.isin(test_set_ids)].sort_index()
     train_y, val_y, test_y = train_y.to_numpy(), val_y.to_numpy(), test_y.to_numpy()
 
     data = {
@@ -321,6 +325,7 @@ def gene_physionet2012(artificially_missing: bool = True):
         "val_y": val_y.flatten(),
         "test_X": test_X,
         "test_y": test_y.flatten(),
+        "scaler": scaler,
     }
 
     if artificially_missing:
