362f16d62c312f2d5cbff69c412aa4fe000e75a7
Wenjie Du <wenjay.du@gmail.com>
2023-05-16 19:54:32 +0800

    feat: add test cases for package `pypots.optim`;

--- pypots/optim/__init__.py
+++ pypots/optim/__init__.py
@@ -6,8 +6,16 @@ Optimizers for PyPOTS NN models.
 # License: GLP-v3
 
 
+from pypots.optim.adagrad import Adagrad
 from pypots.optim.adam import Adam
+from pypots.optim.adamw import AdamW
+from pypots.optim.rmsprop import RMSprop
+from pypots.optim.sgd import SGD
 
 __all__ = [
     "Adam",
+    "AdamW",
+    "Adagrad",
+    "RMSprop",
+    "SGD",
 ]
