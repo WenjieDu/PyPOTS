873baea5ca6a41812214df82275d5c87708ecefe
Bhargav Vemuri <59896417+vemuribv@users.noreply.github.com>
2023-09-04 20:26:58 -0700

    Update model.py

--- pypots/clustering/vader/model.py
+++ pypots/clustering/vader/model.py
@@ -642,5 +642,4 @@ class VaDER(BaseNNClusterer):
                 clustering_results_collector.append(clustering_results)
 
         clustering_results = np.concatenate(clustering_results_collector)
-        return {'clustering results':clustering_results,
-               'results':results}
+        return clustering_results
