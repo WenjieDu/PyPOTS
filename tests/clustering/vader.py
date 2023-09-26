8dfd1892312471ae8ee407d2e1654c3eec4e53b0
Wenjie Du <wenjay.du@gmail.com>
2023-09-26 13:47:18 +0800

    feat: invoke cal_internal_cluster_validation_metrics() in testing cases;

--- tests/clustering/vader.py
+++ tests/clustering/vader.py
@@ -15,7 +15,10 @@ import pytest
 from pypots.clustering import VaDER
 from pypots.optim import Adam
 from pypots.utils.logging import logger
-from pypots.utils.metrics import cal_external_cluster_validation_metrics
+from pypots.utils.metrics import (
+    cal_external_cluster_validation_metrics,
+    cal_internal_cluster_validation_metrics,
+)
 from tests.clustering.config import (
     EPOCHS,
     TRAIN_SET,
@@ -60,11 +63,17 @@ class TestVaDER(unittest.TestCase):
     @pytest.mark.xdist_group(name="clustering-vader")
     def test_1_cluster(self):
         try:
-            clustering = self.vader.cluster(TEST_SET)
+            clustering, latent_collector = self.vader.cluster(
+                TEST_SET, return_latent=True
+            )
             external_metrics = cal_external_cluster_validation_metrics(
                 clustering, DATA["test_y"]
             )
+            internal_metrics = cal_internal_cluster_validation_metrics(
+                latent_collector["z"], DATA["test_y"]
+            )
             logger.info(f"{external_metrics}")
+            logger.info(f"{internal_metrics}")
         except np.linalg.LinAlgError as e:
             logger.error(
                 f"{e}\n"
