496b322f1b4556734ffed4a06df1728446568d63
Wenjie Du <wenjay.du@gmail.com>
2023-11-21 16:02:15 +0800

    fix: copy test set data to avoid affecting other models' testing cases;

--- tests/imputation/locf.py
+++ tests/imputation/locf.py
@@ -32,7 +32,6 @@ class TestLOCF(unittest.TestCase):
 
     @pytest.mark.xdist_group(name="imputation-locf")
     def test_0_impute(self):
-        TEST_SET["X"][0, 0, 0] = np.nan
         # if input data is numpy ndarray
         test_X_imputed_zero = self.locf_zero.predict(TEST_SET)["imputation"]
         assert not np.isnan(
@@ -71,43 +70,42 @@ class TestLOCF(unittest.TestCase):
         logger.info(f"LOCF (nan) still have {num_of_missing} missing values.")
 
         # if input data is torch tensor
-        TEST_SET["X"] = torch.from_numpy(TEST_SET["X"])
-        DATA["test_X_intact"] = torch.from_numpy(DATA["test_X_intact"])
-        DATA["test_X_indicating_mask"] = torch.from_numpy(
-            DATA["test_X_indicating_mask"]
+        X = torch.from_numpy(np.copy(TEST_SET["X"]))
+        test_X_intact = torch.from_numpy(np.copy(DATA["test_X_intact"]))
+        test_X_indicating_mask = torch.from_numpy(
+            np.copy(DATA["test_X_indicating_mask"])
         )
-        test_X_imputed_zero = self.locf_zero.predict(TEST_SET)["imputation"]
+
+        test_X_imputed_zero = self.locf_zero.predict({"X": X})["imputation"]
         assert not torch.isnan(
             test_X_imputed_zero
         ).any(), "Output still has missing values after running impute()."
-        test_MAE = cal_mae(
-            test_X_imputed_zero, DATA["test_X_intact"], DATA["test_X_indicating_mask"]
-        )
+        test_MAE = cal_mae(test_X_imputed_zero, test_X_intact, test_X_indicating_mask)
         logger.info(f"LOCF (zero) test_MAE: {test_MAE}")
 
-        test_X_imputed_backward = self.locf_backward.predict(TEST_SET)["imputation"]
+        test_X_imputed_backward = self.locf_backward.predict({"X": X})["imputation"]
         assert not torch.isnan(
             test_X_imputed_backward
         ).any(), "Output still has missing values after running impute()."
         test_MAE = cal_mae(
             test_X_imputed_backward,
-            DATA["test_X_intact"],
-            DATA["test_X_indicating_mask"],
+            test_X_intact,
+            test_X_indicating_mask,
         )
         logger.info(f"LOCF (backward) test_MAE: {test_MAE}")
 
-        test_X_imputed_mean = self.locf_mean.predict(TEST_SET)["imputation"]
+        test_X_imputed_mean = self.locf_mean.predict({"X": X})["imputation"]
         assert not torch.isnan(
             test_X_imputed_mean
         ).any(), "Output still has missing values after running impute()."
         test_MAE = cal_mae(
             test_X_imputed_mean,
-            DATA["test_X_intact"],
-            DATA["test_X_indicating_mask"],
+            test_X_intact,
+            test_X_indicating_mask,
         )
         logger.info(f"LOCF (mean) test_MAE: {test_MAE}")
 
-        test_X_imputed_nan = self.locf_nan.predict(TEST_SET)["imputation"]
+        test_X_imputed_nan = self.locf_nan.predict({"X": X})["imputation"]
         num_of_missing = torch.isnan(test_X_imputed_nan).sum()
         assert num_of_missing > 0, "Output should have missing data but not."
         logger.info(f"LOCF (nan) still have {num_of_missing} missing values.")
