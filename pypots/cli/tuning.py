3ae553b975e78c34fe6554e01f0695f39922b633
Wenjie Du <wenjay.du@gmail.com>
2023-12-09 00:08:10 +0800

    docs: add some logging and comments;

--- pypots/cli/tuning.py
+++ pypots/cli/tuning.py
@@ -130,9 +130,15 @@ class TuningCommand(BaseCommand):
             tuner_params = nni.get_next_parameter()
             # get the specified model class
             if self._model not in NN_MODELS:
+                logger.info(
+                    f"The specified model {self._model} is not in PyPOTS. "
+                    f"Trying to fetch it from the given model package {self._model_package_path}."
+                )
                 assert self._model_package_path is not None, (
-                    f"The given model {self._model} is not in PyPOTS package. "
-                    f"Please specify the path to the model package with argument `--model_package_path`."
+                    f"The given model {self._model} is not in PyPOTS. "
+                    f"Please give the full import path of the model in PyPOTS like pypots.imputation.SAITS\n"
+                    f"If you're trying to tune an outside model, "
+                    f"please specify the path to the model package with argument `--model_package_path`."
                 )
                 model_package = load_package_from_path(self._model_package_path)
                 assert self._model in model_package.__all__, (
@@ -141,6 +147,13 @@ class TuningCommand(BaseCommand):
                 )
                 model_class = getattr(model_package, self._model)
             else:
+                if self._model_package_path is not None:
+                    logger.warning(
+                        f"‼️ Find the specified model {self._model} in PyPOTS, "
+                        f"but also find the argument --model_package_path is not None."
+                        f"Note that --model_package_path is ignored."
+                    )
+
                 model_class = NN_MODELS[self._model]
             # pop out the learning rate
             lr = tuner_params.pop("lr")
