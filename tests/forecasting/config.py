6fe27ac55342eaa847138a1682b2e42132fe3b35
Wenjie Du <wenjay.du@gmail.com>
2023-12-16 22:29:47 +0800

    fix: fix some testing errors;

--- tests/forecasting/config.py
+++ tests/forecasting/config.py
@@ -5,12 +5,7 @@ Test configs for forecasting models.
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
-import os
-
-from tests.global_test_config import (
-    DATA,
-    RESULT_SAVING_DIR,
-)
+from tests.global_test_config import DATA
 
 EPOCHS = 5
 N_PRED_STEP = 4
@@ -19,5 +14,3 @@ TRAIN_SET = {"X": DATA["train_X"]}
 VAL_SET = {"X": DATA["val_X"]}
 TEST_SET = {"X": DATA["test_X"][:, :-N_PRED_STEP]}
 TEST_SET_INTACT = {"X": DATA["test_X_intact"]}
-
-RESULT_SAVING_DIR_FOR_CLASSIFICATION = os.path.join(RESULT_SAVING_DIR, "forecasting")
