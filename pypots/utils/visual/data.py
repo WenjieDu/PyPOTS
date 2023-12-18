d679d73aa7871bf7167921e97d672d22c255fb9a
Wenjie Du <wenjay.du@gmail.com>
2023-12-18 14:15:01 +0800

    feat: write unit testing cases for pypots.utils.visual.data;

--- pypots/utils/visual/data.py
+++ pypots/utils/visual/data.py
@@ -5,19 +5,23 @@ Utilities for data visualization.
 # Created by Jun Wang <jwangfx@connect.ust.hk> and Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
+from typing import Optional
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from ..logging import logger
+
 
 def plot_data(
-    vals_obs,
-    vals_eval,
-    vals_imputed,
-    dataidx: int = None,
-    nrows: int = 10,
-    ncols: int = 4,
-    figsize=None,
+    X: np.ndarray,
+    X_intact: np.ndarray,
+    X_imputed: np.ndarray,
+    sample_idx: Optional[int] = None,
+    n_rows: int = 10,
+    n_cols: int = 4,
+    fig_size: Optional[list] = None,
 ):
     """Plot the imputed values, the observed values, and the evaluated values of one multivariate timeseries.
     The observed values are marked with red 'x',  the evaluated values are marked with blue 'o',
@@ -25,48 +29,58 @@ def plot_data(
 
     Parameters
     ----------
-    vals_obs : ndarray,
+    X : ndarray,
         The observed values
 
-    vals_eval : ndarray,
+    X_intact : ndarray,
         The evaluated values
 
-    vals_imputed : ndarray,
+    X_imputed : ndarray,
         The imputed values
 
-    dataidx : int,
-        The index of the sample to be plotted
+    sample_idx : int,
+        The index of the sample to be plotted.
+        If None, a randomly-selected sample will be plotted for visualization.
 
-    nrows : int,
+    n_rows : int,
         The number of rows in the plot
 
-    ncols : int,
+    n_cols : int,
         The number of columns in the plot
 
-    figsize : list,
+    fig_size : list,
         The size of the figure
     """
 
-    n_samples, n_steps, n_features = vals_obs.shape
+    vals_shape = X.shape
+    assert (
+        len(vals_shape) == 3
+    ), "vals_obs should be a 3D array of shape (n_samples, n_steps, n_features)"
+    n_samples, n_steps, n_features = vals_shape
 
-    if dataidx is None:
-        dataidx = np.random.randint(low=0, high=n_samples)
-    if figsize is None:
-        figsize = [24, 36]
+    if sample_idx is None:
+        sample_idx = np.random.randint(low=0, high=n_samples)
+        logger.warning(
+            f"No sample index is specified, a random sample {sample_idx} is selected for visualization."
+        )
 
-    n_k = nrows * ncols
+    if fig_size is None:
+        fig_size = [24, 36]
+
+    n_k = n_rows * n_cols
     K = np.min([n_features, n_k])
     L = n_steps
     plt.rcParams["font.size"] = 16
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(figsize[0], figsize[1]))
-    # fig.delaxes(axes[-1][-1])
+    fig, axes = plt.subplots(
+        nrows=n_rows, ncols=n_cols, figsize=(fig_size[0], fig_size[1])
+    )
 
     for k in range(K):
-        df = pd.DataFrame({"x": np.arange(0, L), "val": vals_imputed[dataidx, :, k]})
-        df1 = pd.DataFrame({"x": np.arange(0, L), "val": vals_obs[dataidx, :, k]})
-        df2 = pd.DataFrame({"x": np.arange(0, L), "val": vals_eval[dataidx, :, k]})
-        row = k // ncols
-        col = k % ncols
+        df = pd.DataFrame({"x": np.arange(0, L), "val": X_imputed[sample_idx, :, k]})
+        df1 = pd.DataFrame({"x": np.arange(0, L), "val": X[sample_idx, :, k]})
+        df2 = pd.DataFrame({"x": np.arange(0, L), "val": X_intact[sample_idx, :, k]})
+        row = k // n_cols
+        col = k % n_cols
         axes[row][col].plot(df1.x, df1.val, color="r", marker="x", linestyle="None")
         axes[row][col].plot(df2.x, df2.val, color="b", marker="o", linestyle="None")
         axes[row][col].plot(df.x, df.val, color="g", linestyle="solid")
@@ -74,58 +88,80 @@ def plot_data(
             plt.setp(axes[row, 0], ylabel="value")
         if row == -1:
             plt.setp(axes[-1, col], xlabel="time")
+    plt.show()
 
 
-def plot_missingness(mask, t_max=1, t_min=0, dataidx=None):
+def plot_missingness(
+    missing_mask,
+    min_step=0,
+    max_step=1,
+    sample_idx: Optional[int] = None,
+):
     """Plot the missingness pattern of one multivariate timeseries. For each feature,
     the observed timestamp is marked with blue '|'. The distribution of sequence lengths is also plotted.
     Hereby, the sequence length is defined as the number of observed timestamps in one feature.
 
     Parameters
     ----------
-    mask : ndarray,
-        The mask matrix of one multivariate timeseries
+    missing_mask : ndarray,
+        The missing mask of multivariate time series.
 
-    t_max : int,
-        The maximum time
+    min_step : int,
+        The minimum time step for visualization.
 
-    t_min : int,
-        The minimum time
+    max_step : int,
+        The maximum time step for visualization.
 
-    dataidx : int,
-        The index of the sample to be plotted
+    sample_idx : int,
+        The index of the sample to be plotted, if None, a randomly-selected sample will be plotted for visualization.
     """
-    n_s, n_l, n_c = mask.shape
+    mask_shape = missing_mask.shape
+    if len(mask_shape) == 3:
+        n_samples, n_steps, n_features = missing_mask.shape
+        if sample_idx is None:
+            sample_idx = np.random.randint(low=0, high=n_samples)
+            logger.warning(
+                f"No sample index is specified, a random sample {sample_idx} is selected for visualization."
+            )
+        mask_sample_for_vis = np.transpose(missing_mask[sample_idx], (1, 0))
+    elif len(mask_shape) == 2:
+        n_steps, n_features = missing_mask.shape
+        mask_sample_for_vis = np.transpose(missing_mask, (1, 0))
+    else:
+        raise ValueError(
+            f"missing_mask should be missing masks of multiple time series samples of "
+            f"shape (n_samples, n_steps, n_features), "
+            f"or of a single time series sample of shape (n_steps, n_features). "
+            f"But got invalid shape of missing_mask: {mask_shape}."
+        )
+
     time = np.repeat(
-        np.repeat(np.linspace(0, t_max, n_l).reshape(1, n_l, 1), axis=2, repeats=n_c),
-        axis=0,
-        repeats=n_s,
+        np.linspace(0, max_step, n_steps).reshape(n_steps, 1),
+        axis=1,
+        repeats=n_features,
     )
-    if dataidx is None:
-        dataidx = np.random.randint(low=0, high=n_s)
+    plot_sample = np.transpose(time, (1, 0))
 
     fig, axes = plt.subplots(figsize=[12, 3.5], dpi=200, nrows=1, ncols=2)
     plt.subplots_adjust(hspace=0.1)
     seq_len = []
-    sample = np.transpose(time[dataidx], (1, 0))
-    mask_s = np.transpose(mask[dataidx], (1, 0))
-    for feature_idx in range(n_c):
-        t = sample[feature_idx][mask_s[feature_idx] == 1]
-        axes[0].scatter(t, np.ones_like(t) * (feature_idx), alpha=1, c="C0", marker="|")
+    for feature_idx in range(n_features):
+        t = plot_sample[feature_idx][mask_sample_for_vis[feature_idx] == 1]
+        axes[0].scatter(t, np.ones_like(t) * feature_idx, alpha=1, c="C0", marker="|")
         seq_len.append(len(t))
     axes[0].set_title("Visualization of arrival times", fontsize=9)
     axes[0].set_xlabel("Time", fontsize=7)
     axes[0].set_ylabel("Features #", fontsize=7)
-    axes[0].set_xlim(-1, n_l)
-    # axes[0].set_ylim(0, n_c-1)
+    axes[0].set_xlim(-1, n_steps)
+    # axes[0].set_ylim(0, n_features-1)
     axes[0].tick_params(axis="both", labelsize=7)
 
     axes[1].set_title("Distribution of sequence lengths", fontsize=9)
     axes[1].hist(
         seq_len,
-        bins=n_l,
+        bins=n_steps,
         color="C1",
-        range=(t_min, t_max),
+        range=(min_step, max_step),
     )
     axes[1].set_xlabel(r"Sequence length", fontsize=7)
     axes[1].set_ylabel("Frequency", fontsize=7)
