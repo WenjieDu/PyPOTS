1929e178b89182b2dc416cb7f2230533489ae1b0
Wenjie Du <wenjay.du@gmail.com>
2023-12-20 00:26:18 +0800

    feat: add load_dict_from_h5();

--- pypots/data/saving/__init__.py
+++ pypots/data/saving/__init__.py
@@ -5,11 +5,12 @@ Data saving utilities.
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
-from .h5 import save_dict_into_h5
+from .h5 import save_dict_into_h5, load_dict_from_h5
 from .pickle import pickle_dump, pickle_load
 
 __all__ = [
     "save_dict_into_h5",
+    "load_dict_from_h5",
     "pickle_dump",
     "pickle_load",
 ]
