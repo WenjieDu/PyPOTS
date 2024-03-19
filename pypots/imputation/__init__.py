8bcee52b3e1b0938b7a5e14cd84217b5ee0b43bb
Wenjie Du <wenjay.du@gmail.com>
2024-03-19 15:32:11 +0800

    feat: add naive imputation methods, Mean and Median;

--- pypots/imputation/__init__.py
+++ pypots/imputation/__init__.py
@@ -5,24 +5,33 @@ Expose all usable time-series imputation models.
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
+# neural network imputation methods
 from .brits import BRITS
 from .csdi import CSDI
 from .gpvae import GPVAE
-from .locf import LOCF
 from .mrnn import MRNN
 from .saits import SAITS
 from .timesnet import TimesNet
 from .transformer import Transformer
 from .usgan import USGAN
 
+# naive imputation methods
+from .locf import LOCF
+from .mean import Mean
+from .median import Median
+
 __all__ = [
+    # neural network imputation methods
     "SAITS",
     "Transformer",
     "TimesNet",
     "BRITS",
     "MRNN",
-    "LOCF",
     "GPVAE",
     "USGAN",
     "CSDI",
+    # naive imputation methods
+    "LOCF",
+    "Mean",
+    "Median",
 ]
