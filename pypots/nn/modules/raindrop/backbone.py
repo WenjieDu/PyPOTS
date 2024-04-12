8483d8fe95165d7adb4d4a83e62f93883786b945
Wenjie Du <wenjay.du@gmail.com>
2024-04-12 22:38:00 +0800

    refactor: modularize Raindrop;

--- pypots/nn/modules/raindrop/backbone.py
+++ pypots/nn/modules/raindrop/backbone.py
@@ -11,6 +11,8 @@ Graph-Guided Network for Irregularly Sampled Multivariate Time Series. ICLR 2022
 # License: BSD-3-Clause
 
 
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -20,7 +22,7 @@ from torch.nn.parameter import Parameter
 from ....utils.logging import logger
 
 try:
-    from .submodules import PositionalEncoding, ObservationPropagation
+    from .layers import PositionalEncoding, ObservationPropagation
     from torch_geometric.nn.inits import glorot
 except ImportError as e:
     logger.error(
@@ -38,7 +40,7 @@ except NameError as e:
     )
 
 
-class _Raindrop(nn.Module):
+class BackboneRaindrop(nn.Module):
     def __init__(
         self,
         n_features,
@@ -50,10 +52,10 @@ class _Raindrop(nn.Module):
         dropout=0.3,
         max_len=215,
         d_static=9,
+        d_pe=16,
         aggregation="mean",
         sensor_wise_mask=False,
         static=False,
-        device=None,
     ):
         super().__init__()
         self.n_layers = n_layers
@@ -68,7 +70,6 @@ class _Raindrop(nn.Module):
         self.aggregation = aggregation
         self.sensor_wise_mask = sensor_wise_mask
         self.static = static
-        self.device = device
 
         # create modules
         if self.static:
@@ -78,7 +79,7 @@ class _Raindrop(nn.Module):
         assert d_model % n_features == 0, "d_model must be divisible by n_features"
         self.d_ob = int(d_model / n_features)
         self.encoder = nn.Linear(n_features * self.d_ob, n_features * self.d_ob)
-        d_pe = 16
+
         self.pos_encoder = PositionalEncoding(d_pe, max_len)
         if self.sensor_wise_mask:
             dim_check = n_features * (self.d_ob + d_pe)
@@ -121,7 +122,7 @@ class _Raindrop(nn.Module):
             nn.Linear(d_final, n_classes),
         )
 
-        self.dropout = nn.Dropout(dropout)
+        self.dropout_layer = nn.Dropout(dropout)
         self.init_weights()
 
     def init_weights(self):
@@ -131,62 +132,66 @@ class _Raindrop(nn.Module):
             self.static_emb.weight.data.uniform_(-init_range, init_range)
         glorot(self.R_u)
 
-    def classify(self, inputs: dict) -> torch.Tensor:
+    def forward(
+        self,
+        X: torch.Tensor,
+        timestamps: torch.Tensor,
+        lengths: torch.Tensor,
+    ) -> Tuple[torch.Tensor, ...]:
         """Forward processing of BRITS.
 
         Parameters
         ----------
-        inputs : dict,
-            The input data.
+        X :
+            The input tensor of shape (batch_size, n_features, max_len).
+
+        timestamps :
+            The timestamps tensor of shape (batch_size, max_len).
+
+        lengths :
+            The lengths tensor of shape (batch_size, 1).
 
         Returns
         -------
         prediction : torch.Tensor
         """
-        src = inputs["X"].permute(1, 0, 2)
-        static = inputs["static"]
-        times = inputs["timestamps"].permute(1, 0)
-        lengths = inputs["lengths"]
-        missing_mask = inputs["missing_mask"].permute(1, 0, 2)
+        src = X.permute(1, 0, 2)
+        times = timestamps.permute(1, 0)
 
+        device = src.device
         max_len, batch_size = src.shape[0], src.shape[1]
 
         src = torch.repeat_interleave(src, self.d_ob, dim=-1)
         h = F.relu(src * self.R_u)
-        pe = self.pos_encoder(times).to(src.device)
-        if static is not None:
-            emb = self.static_emb(static)
-
-        h = self.dropout(h)
+        pe = self.pos_encoder(times).to(device)
+        h = self.dropout_layer(h)
 
         mask = torch.arange(max_len)[None, :] >= (lengths.cpu()[:, None])
-        mask = mask.squeeze(1).to(src.device)
+        mask = mask.squeeze(1).to(device)
 
         x = h
 
-        adj = torch.ones(self.n_features, self.n_features, device=src.device)
+        adj = torch.ones(self.n_features, self.n_features, device=device)
         adj[torch.eye(self.n_features, dtype=torch.bool)] = 1
 
         edge_index = torch.nonzero(adj).T
         edge_weights = adj[edge_index[0], edge_index[1]]
 
-        batch_size = src.shape[1]
-        n_step = src.shape[0]
         output = torch.zeros(
-            [n_step, batch_size, self.n_features * self.d_ob], device=src.device
+            [max_len, batch_size, self.n_features * self.d_ob], device=device
         )
 
-        alpha_all = torch.zeros([edge_index.shape[1], batch_size], device=src.device)
+        alpha_all = torch.zeros([edge_index.shape[1], batch_size], device=device)
 
         # iterate on each sample
         for unit in range(0, batch_size):
             step_data = x[:, unit, :]
             p_t = pe[:, unit, :]
 
-            step_data = step_data.reshape([n_step, self.n_features, self.d_ob]).permute(
-                1, 0, 2
-            )
-            step_data = step_data.reshape(self.n_features, n_step * self.d_ob)
+            step_data = step_data.reshape(
+                [max_len, self.n_features, self.d_ob]
+            ).permute(1, 0, 2)
+            step_data = step_data.reshape(self.n_features, max_len * self.d_ob)
 
             step_data, attention_weights = self.ob_propagation(
                 step_data,
@@ -211,7 +216,7 @@ class _Raindrop(nn.Module):
                 return_attention_weights=True,
             )
 
-            step_data = step_data.view([self.n_features, n_step, self.d_ob])
+            step_data = step_data.view([self.n_features, max_len, self.d_ob])
             step_data = step_data.permute([1, 0, 2])  # [n_step, n_features, d_ob]
             step_data = step_data.reshape([-1, self.n_features * self.d_ob])
 
@@ -229,48 +234,5 @@ class _Raindrop(nn.Module):
         else:
             output = torch.cat([output, pe], dim=2)
 
-        r_out = self.transformer_encoder(output, src_key_padding_mask=mask)
-
-        lengths2 = lengths.unsqueeze(1).to(src.device)
-        mask2 = mask.permute(1, 0).unsqueeze(2).long()
-        if self.sensor_wise_mask:
-            output = torch.zeros(
-                [batch_size, self.n_features, self.d_ob + 16], device=src.device
-            )
-            extended_missing_mask = missing_mask.view(-1, batch_size, self.n_features)
-            for se in range(self.n_features):
-                r_out = r_out.view(-1, batch_size, self.n_features, (self.d_ob + 16))
-                out = r_out[:, :, se, :]
-                l_ = torch.sum(extended_missing_mask[:, :, se], dim=0).unsqueeze(
-                    1
-                )  # length
-                out_sensor = torch.sum(
-                    out * (1 - extended_missing_mask[:, :, se].unsqueeze(-1)), dim=0
-                ) / (l_ + 1)
-                output[:, se, :] = out_sensor
-            output = output.view([-1, self.n_features * (self.d_ob + 16)])
-        elif self.aggregation == "mean":
-            output = torch.sum(r_out * (1 - mask2), dim=0) / (lengths2 + 1)
-        else:
-            raise RuntimeError
-
-        if static is not None:
-            output = torch.cat([output, emb], dim=1)
-
-        logits = self.mlp_static(output)
-        prediction = torch.softmax(logits, dim=1)
-
-        return prediction
-
-    def forward(self, inputs, training=True):
-        classification_pred = self.classify(inputs)
-        results = {"classification_pred": classification_pred}
-
-        # if in training mode, return results with losses
-        if training:
-            classification_loss = F.nll_loss(
-                torch.log(classification_pred), inputs["label"]
-            )
-            results["loss"] = classification_loss
-
-        return results
+        output = self.transformer_encoder(output, src_key_padding_mask=mask)
+        return output, mask
