ee39fc106b54f74a7e835e8f498d92611d0b5ecd
Wenjie Du <wenjay.du@gmail.com>
2024-05-17 14:57:04 +0800

    docs: update docs;

--- pypots/imputation/stemgnn/__init__.py
+++ pypots/imputation/stemgnn/__init__.py
@@ -2,7 +2,8 @@
 The package of the partially-observed time-series imputation model StemGNN.
 
 Refer to the paper
-`Defu Cao, Yujing Wang, Juanyong Duan, Ce Zhang, Xia Zhu, Congrui Huang, Yunhai Tong, Bixiong Xu, Jing Bai, Jie Tong, Qi Zhang.
+`Defu Cao, Yujing Wang, Juanyong Duan, Ce Zhang, Xia Zhu,
+Congrui Huang, Yunhai Tong, Bixiong Xu, Jing Bai, Jie Tong, Qi Zhang.
 "Spectral Temporal Graph Neural Network for Multivariate Time-series Forecasting".
 In Advances in Neural Information Processing Systems, 2020.
 <https://proceedings.neurips.cc/paper_files/paper/2020/file/cdf6581cb7aca4b7e19ef136c6e601a5-Paper.pdf>`_
