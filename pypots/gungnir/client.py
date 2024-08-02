b9f3016a6fa1617a55e88916c2bb6f7cb999096e
Wenjie Du <wenjay.du@gmail.com>
2024-08-02 21:17:33 +0800

    refactor: logging when init Gungnir;

--- pypots/gungnir/client.py
+++ pypots/gungnir/client.py
@@ -7,12 +7,6 @@
 
 from ..utils.logging import logger
 
-logger.info(
-    "The functionalities of client Gungnir have not been implemented yet,\n"
-    "which will be updated later 2024 once our close beta test is ready.\n"
-    "AI for real-world time series is coming. 🚀 Stay tuned please! https://time-series.ai"
-)
-
 
 class TimeSeriesAI:
     def __init__(self):
@@ -22,3 +16,8 @@ class TimeSeriesAI:
 class Gungnir(TimeSeriesAI):
     def __init__(self):
         super().__init__()
+        logger.info(
+            "The functionalities of client Gungnir have not been implemented yet,\n"
+            "which will be updated later 2024 once our close beta test is ready.\n"
+            "AI for real-world time series is coming. 🚀 Stay tuned please! https://time-series.ai"
+        )
