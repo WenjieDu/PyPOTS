455e380950a2cfb5020fd1b179f9ce02c6a4d743
Wenjie Du <wenjay.du@gmail.com>
2024-09-12 23:51:36 +0800

    test

--- pypots/gungnir/client.py
+++ pypots/gungnir/client.py
@@ -5,8 +5,9 @@
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
+from ..utils.logging import logger
 
-print(
+logger.info(
     """
 ████████╗██╗███╗   ███╗███████╗    ███████╗███████╗██████╗ ██╗███████╗███████╗    █████╗ ██╗
 ╚══██╔══╝██║████╗ ████║██╔════╝    ██╔════╝██╔════╝██╔══██╗██║██╔════╝██╔════╝   ██╔══██╗██║
