054b3c9b60d9032fa17350d2fd00f1af68cddecd
Wenjie Du <wenjay.du@gmail.com>
2024-12-02 12:07:23 +0800

    refactor: add customized loss and metric funcs for CSAI models;

--- pypots/classification/csai/model.py
+++ pypots/classification/csai/model.py
@@ -70,6 +70,14 @@ class CSAI(BaseNNClassifier):
         stopped when the model does not perform better after that number of epochs.
         Leaving it default as None will disable the early-stopping.
 
+    train_loss_func:
+        The customized loss function designed by users for training the model.
+        If not given, will use the default loss as claimed in the original paper.
+
+    val_metric_func:
+        The customized metric function designed by users for validating the model.
+        If not given, will use the default loss as metric as claimed in the original paper.
+
     optimizer :
         The optimizer for model training.
         If not given, will use a default Adam optimizer.
@@ -120,6 +128,8 @@ class CSAI(BaseNNClassifier):
         batch_size: int = 32,
         epochs: int = 100,
         patience: Optional[int] = None,
+        train_loss_func: Optional[dict] = None,
+        val_metric_func: Optional[dict] = None,
         optimizer: Optimizer = Adam(),
         num_workers: int = 0,
         device: Optional[Union[str, torch.device, list]] = None,
@@ -128,15 +138,17 @@ class CSAI(BaseNNClassifier):
         verbose: bool = True,
     ):
         super().__init__(
-            n_classes,
-            batch_size,
-            epochs,
-            patience,
-            num_workers,
-            device,
-            saving_path,
-            model_saving_strategy,
-            verbose,
+            n_classes=n_classes,
+            batch_size=batch_size,
+            epochs=epochs,
+            patience=patience,
+            train_loss_func=train_loss_func,
+            val_metric_func=val_metric_func,
+            num_workers=num_workers,
+            device=device,
+            saving_path=saving_path,
+            model_saving_strategy=model_saving_strategy,
+            verbose=verbose,
         )
 
         self.n_steps = n_steps
