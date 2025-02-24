cd9fffd3f9464f8b8908d8380399e8dc070936b6
Wenjie Du <wenjay.du@gmail.com>
2025-02-24 13:09:15 +0800

    refactor: remove data preprocessing funcs which are in benchpots now;

--- pypots/data/__init__.py
+++ pypots/data/__init__.py
@@ -6,7 +6,6 @@ Expose all usable data manipulation classes and functions.
 # License: BSD-3-Clause
 
 from .dataset import BaseDataset, SUPPORTED_DATASET_FILE_FORMATS
-from .generating import gene_random_walk
 from .saving import (
     save_dict_into_h5,
     load_dict_from_h5,
@@ -18,15 +17,11 @@ from .utils import (
     sliding_window,
     inverse_sliding_window,
 )
-from .load_specific_datasets import load_specific_dataset
 
 __all__ = [
     # base dataset classes
     "BaseDataset",
     "SUPPORTED_DATASET_FILE_FORMATS",
-    # dataset generation functions
-    "gene_random_walk",
-    "load_specific_dataset",
     # utils
     "parse_delta",
     "sliding_window",
