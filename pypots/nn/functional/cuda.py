707a009723b24fdcb9ae5469913bfbc5f0cebd85
Wenjie Du <wenjay.du@gmail.com>
2025-03-02 21:34:09 +0800

    feat: add pypots.nn.functional.cuda;

---
+++ pypots/nn/functional/cuda.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+
+import torch
+
+
+# overwrite autocast to make it compatible with both torch >=2.4 and <2.4
+def autocast(**kwargs):
+    if torch.__version__ >= "2.4":
+        from torch.cuda.amp import autocast
+
+        return autocast(**kwargs)
+    else:
+        from torch.amp import autocast
+
+        return autocast("cuda", **kwargs)
--- pypots/nn/modules/reformer/local_attention.py
+++ pypots/nn/modules/reformer/local_attention.py
@@ -14,19 +14,9 @@ from einops import rearrange
 from einops import repeat, pack, unpack
 from torch import nn, einsum
 
+from ...functional.cuda import autocast
+
 TOKEN_SELF_ATTN_VALUE = -5e4
-
-
-# overwrite autocast to make it compatible with both torch >=2.4 and <2.4
-def autocast(**kwargs):
-    if torch.__version__ >= "2.4":
-        from torch.cuda.amp import autocast
-
-        return autocast(**kwargs)
-    else:
-        from torch.amp import autocast
-
-        return autocast("cuda", **kwargs)
 
 
 def exists(val):
