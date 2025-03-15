670ff12cff524677a89cf3435f37d9a99fe9d1b0
Wenjie Du <wenjay.du@gmail.com>
2025-03-15 18:36:42 +0800

    feat: add modules of TS2Vec;

---
+++ pypots/nn/modules/ts2vec/backbone.py
@@ -0,0 +1,190 @@
+"""
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+from typing import Optional
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from .layers import DilatedConvEncoder
+from .utils import (
+    torch_pad_nan,
+    generate_binomial_mask,
+    generate_continuous_mask,
+)
+
+MASK_MODES = ["binomial", "continuous", "all_true", "all_false", "mask_last"]
+
+
+class TS2VecEncoder(nn.Module):
+    def __init__(
+        self,
+        n_features: int,
+        n_pred_features: int,
+        d_hidden: int,
+        n_layers: int,
+        mask_mode: str = "binomial",
+    ):
+        super().__init__()
+        assert mask_mode in MASK_MODES, f"mask_mode should be one of {MASK_MODES}"
+
+        self.n_features = n_features
+        self.n_pred_features = n_pred_features
+        self.d_hidden = d_hidden
+        self.mask_mode = mask_mode
+        self.input_fc = nn.Linear(n_features, d_hidden)
+        self.feature_extractor = DilatedConvEncoder(d_hidden, [d_hidden] * n_layers + [n_pred_features], kernel_size=3)
+        self.repr_dropout = nn.Dropout(p=0.1)
+
+    def forward(self, x, mask=None):  # x: B x T x n_features
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch
+
+        # generate & apply mask
+        if mask is None:
+            if self.training:
+                mask = self.mask_mode
+            else:
+                mask = "all_true"
+
+        if mask == "binomial":
+            mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == "continuous":
+            mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == "all_true":
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+        elif mask == "all_false":
+            mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
+        elif mask == "mask_last":
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+            mask[:, -1] = False
+
+        mask &= nan_mask
+        x[~mask] = 0
+
+        # conv encoder
+        x = x.transpose(1, 2)  # B x Ch x T
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B x T x Co
+
+        return x
+
+    def _eval_with_pooling(self, x, mask=None, slicing=None, encoding_window=None):
+        out = self.forward(x, mask)
+        if encoding_window == "full_series":
+            if slicing is not None:
+                out = out[:, slicing]
+            out = F.max_pool1d(
+                out.transpose(1, 2),
+                kernel_size=out.size(1),
+            ).transpose(1, 2)
+
+        elif isinstance(encoding_window, int):
+            out = F.max_pool1d(
+                out.transpose(1, 2), kernel_size=encoding_window, stride=1, padding=encoding_window // 2
+            ).transpose(1, 2)
+            if encoding_window % 2 == 0:
+                out = out[:, :-1]
+            if slicing is not None:
+                out = out[:, slicing]
+
+        elif encoding_window == "multiscale":
+            p = 0
+            reprs = []
+            while (1 << p) + 1 < out.size(1):
+                t_out = F.max_pool1d(
+                    out.transpose(1, 2), kernel_size=(1 << (p + 1)) + 1, stride=1, padding=1 << p
+                ).transpose(1, 2)
+                if slicing is not None:
+                    t_out = t_out[:, slicing]
+                reprs.append(t_out)
+                p += 1
+            out = torch.cat(reprs, dim=-1)
+
+        else:
+            if slicing is not None:
+                out = out[:, slicing]
+
+        return out.cpu()
+
+    @torch.no_grad()
+    def encode(
+        self,
+        x: torch.Tensor,
+        mask: Optional[str] = None,
+        encoding_window: Optional[int] = None,
+        causal: bool = False,
+        sliding_length: Optional[int] = None,
+        sliding_padding: int = 0,
+    ) -> torch.Tensor:
+        """Compute representations using the trained model.
+
+        Parameters
+        ----------
+        x:
+            This should have a shape of (n_samples, n_steps, n_features). All missing data should be set to NaN.
+
+        mask:
+            The mask used by encoder can be specified with this parameter.
+            This can be set to 'binomial', 'continuous', 'all_true', 'all_false' or 'mask_last'.
+
+        encoding_window:
+            When this param is specified, the computed representation would the max pooling over this window.
+            This can be set to 'full_series', 'multiscale' or an integer specifying the pooling kernel size.
+
+        causal:
+            When this param is set to True, the future information would not be encoded into representation of
+            each timestamp.
+
+        sliding_length:
+            The length of sliding window. When this param is specified,
+            a sliding inference would be applied on the time series.
+
+        sliding_padding:
+            This param specifies the contextual data length used for inference every sliding windows.
+
+        Returns
+        -------
+            repr: The representations for data.
+
+        """
+
+        n_samples, n_steps, _ = x.shape
+
+        if sliding_length is not None:
+            reprs = []
+            for i in range(0, n_steps, sliding_length):
+                l = i - sliding_padding
+                r = i + sliding_length + (sliding_padding if not causal else 0)
+                x_sliding = torch_pad_nan(
+                    x[:, max(l, 0) : min(r, n_steps)],
+                    left=-l if l < 0 else 0,
+                    right=r - n_steps if r > n_steps else 0,
+                    dim=1,
+                )
+                out = self._eval_with_pooling(
+                    x_sliding,
+                    mask,
+                    slicing=slice(sliding_padding, sliding_padding + sliding_length),
+                    encoding_window=encoding_window,
+                )
+                reprs.append(out)
+
+            reprs = torch.cat(reprs, dim=1)
+            if encoding_window == "full_series":
+                reprs = F.max_pool1d(
+                    reprs.transpose(1, 2).contiguous(),
+                    kernel_size=reprs.size(1),
+                ).squeeze(1)
+        else:
+            reprs = self._eval_with_pooling(x, mask, encoding_window=encoding_window)
+            if encoding_window == "full_series":
+                reprs = reprs.squeeze(1)
+
+        return reprs
---
+++ pypots/nn/modules/ts2vec/layers.py
@@ -0,0 +1,61 @@
+"""
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+
+import torch.nn.functional as F
+from torch import nn
+
+
+class SamePadConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1):
+        super().__init__()
+        self.receptive_field = (kernel_size - 1) * dilation + 1
+        padding = self.receptive_field // 2
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, padding=padding, dilation=dilation, groups=groups)
+        self.remove = 1 if self.receptive_field % 2 == 0 else 0
+
+    def forward(self, x):
+        out = self.conv(x)
+        if self.remove > 0:
+            out = out[:, :, : -self.remove]
+        return out
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation, final=False):
+        super().__init__()
+        self.conv1 = SamePadConv(in_channels, out_channels, kernel_size, dilation=dilation)
+        self.conv2 = SamePadConv(out_channels, out_channels, kernel_size, dilation=dilation)
+        self.projector = nn.Conv1d(in_channels, out_channels, 1) if in_channels != out_channels or final else None
+
+    def forward(self, x):
+        residual = x if self.projector is None else self.projector(x)
+        x = F.gelu(x)
+        x = self.conv1(x)
+        x = F.gelu(x)
+        x = self.conv2(x)
+        return x + residual
+
+
+class DilatedConvEncoder(nn.Module):
+    def __init__(self, in_channels, channels, kernel_size):
+        super().__init__()
+        self.net = nn.Sequential(
+            *[
+                ConvBlock(
+                    channels[i - 1] if i > 0 else in_channels,
+                    channels[i],
+                    kernel_size=kernel_size,
+                    dilation=2**i,
+                    final=(i == len(channels) - 1),
+                )
+                for i in range(len(channels))
+            ]
+        )
+
+    def forward(self, x):
+        return self.net(x)
