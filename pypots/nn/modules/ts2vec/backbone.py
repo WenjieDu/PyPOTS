281ee249b31026125649e5a11586c5c651a693dc
Wenjie Du <wenjay.du@gmail.com>
2025-03-17 16:05:09 +0800

    feat: add downstream sklearn models for TS2Vec;

--- pypots/nn/modules/ts2vec/backbone.py
+++ pypots/nn/modules/ts2vec/backbone.py
@@ -7,8 +7,16 @@
 
 from typing import Optional
 
+import numpy as np
 import torch
 import torch.nn.functional as F
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from torch import nn
 
 from .layers import DilatedConvEncoder
@@ -118,7 +126,7 @@ class TS2VecEncoder(nn.Module):
         self,
         x: torch.Tensor,
         mask: Optional[str] = None,
-        encoding_window: Optional[int] = None,
+        encoding_window: Optional[str] = None,
         causal: bool = False,
         sliding_length: Optional[int] = None,
         sliding_padding: int = 0,
@@ -188,3 +196,85 @@ class TS2VecEncoder(nn.Module):
                 reprs = reprs.squeeze(1)
 
         return reprs
+
+    @staticmethod
+    def fit_svm(features, y, MAX_SAMPLES=10000):
+        nb_classes = np.unique(y, return_counts=True)[1].shape[0]
+        train_size = features.shape[0]
+
+        svm = SVC(C=np.inf, gamma="scale")
+        if train_size // nb_classes < 5 or train_size < 50:
+            return svm.fit(features, y)
+        else:
+            grid_search = GridSearchCV(
+                svm,
+                {
+                    "C": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, np.inf],
+                    "kernel": ["rbf"],
+                    "degree": [3],
+                    "gamma": ["scale"],
+                    "coef0": [0],
+                    "shrinking": [True],
+                    "probability": [False],
+                    "tol": [0.001],
+                    "cache_size": [200],
+                    "class_weight": [None],
+                    "verbose": [False],
+                    "max_iter": [10000000],
+                    "decision_function_shape": ["ovr"],
+                    "random_state": [None],
+                },
+                cv=5,
+                n_jobs=5,
+            )
+            # If the training set is too large, subsample MAX_SAMPLES examples
+            if train_size > MAX_SAMPLES:
+                split = train_test_split(features, y, train_size=MAX_SAMPLES, random_state=0, stratify=y)
+                features = split[0]
+                y = split[2]
+
+            grid_search.fit(features, y)
+            return grid_search.best_estimator_
+
+    @staticmethod
+    def fit_lr(features, y, MAX_SAMPLES=100000):
+        # If the training set is too large, subsample MAX_SAMPLES examples
+        if features.shape[0] > MAX_SAMPLES:
+            split = train_test_split(features, y, train_size=MAX_SAMPLES, random_state=0, stratify=y)
+            features = split[0]
+            y = split[2]
+
+        pipe = make_pipeline(StandardScaler(), LogisticRegression(random_state=0, max_iter=1000000, multi_class="ovr"))
+        pipe.fit(features, y)
+        return pipe
+
+    @staticmethod
+    def fit_knn(features, y):
+        pipe = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=1))
+        pipe.fit(features, y)
+        return pipe
+
+    @staticmethod
+    def fit_ridge(train_features, train_y, valid_features, valid_y, MAX_SAMPLES=100000):
+        # If the training set is too large, subsample MAX_SAMPLES examples
+        if train_features.shape[0] > MAX_SAMPLES:
+            split = train_test_split(train_features, train_y, train_size=MAX_SAMPLES, random_state=0)
+            train_features = split[0]
+            train_y = split[2]
+        if valid_features.shape[0] > MAX_SAMPLES:
+            split = train_test_split(valid_features, valid_y, train_size=MAX_SAMPLES, random_state=0)
+            valid_features = split[0]
+            valid_y = split[2]
+
+        alphas = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        valid_results = []
+        for alpha in alphas:
+            lr = Ridge(alpha=alpha).fit(train_features, train_y)
+            valid_pred = lr.predict(valid_features)
+            score = np.sqrt(((valid_pred - valid_y) ** 2).mean()) + np.abs(valid_pred - valid_y).mean()
+            valid_results.append(score)
+        best_alpha = alphas[np.argmin(valid_results)]
+
+        lr = Ridge(alpha=best_alpha)
+        lr.fit(train_features, train_y)
+        return lr
