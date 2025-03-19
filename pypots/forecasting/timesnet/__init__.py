9c6db7ca2d73a72103012010abde68d3904cfbd2
Wenjie Du <wenjay.du@gmail.com>
2025-03-19 11:26:02 +0800

    docs: update docs for forecasting TimesNet;

--- pypots/forecasting/timesnet/__init__.py
+++ pypots/forecasting/timesnet/__init__.py
@@ -1,16 +1,16 @@
 """
-The implementation of TimesNet for the partially-observed time-series forecasting task.
+The package of the partially-observed time-series forecasting model TimesNet.
 
-Refer to the papers
-`Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez, Ł ukasz Kaiser,
-and Illia Polosukhin.
-Attention is all you need.
-In Advances in Neural Information Processing Systems, volume 30. Curran Associates, Inc., 2017.
-<https://proceedings.neurips.cc/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf>`_
+Refer to the paper
+`Haixu Wu, Tengge Hu, Yong Liu, Hang Zhou, Jianmin Wang, and Mingsheng Long.
+TimesNet: Temporal 2D-Variation Modeling for General Time Series Analysis.
+In ICLR, 2023.
+<https://openreview.net/pdf?id=ju_Uqw384Oq>`_
 
 Notes
 -----
-This implementation is inspired by https://github.com/WenjieDu/SAITS
+This implementation is inspired by the official one https://github.com/thuml/Time-Series-Library
+
 """
 
 # Created by Wenjie Du <wenjay.du@gmail.com>
