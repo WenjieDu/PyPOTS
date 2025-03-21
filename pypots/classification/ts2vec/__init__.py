727c417478393a9b181c8aa811ba9ad529e0fba6
Wenjie Du <wenjay.du@gmail.com>
2025-03-21 16:12:54 +0800

    Merge pull request #658 from WenjieDu/(docs)update

--- pypots/classification/ts2vec/__init__.py
+++ pypots/classification/ts2vec/__init__.py
@@ -1,4 +1,15 @@
 """
+The package of the partially-observed time-series classification model TS2Vec.
+
+Refer to the paper
+`Zhihan Yue, Yujing Wang, Juanyong Duan, Tianmeng Yang, Congrui Huang, Yunhai Tong, Bixiong Xu.
+"TS2Vec: Towards Universal Representation of Time Series".
+In AAAI 2022.
+<https://ojs.aaai.org/index.php/AAAI/article/view/20881/20640>`_
+
+Notes
+-----
+This implementation is inspired by the official one https://github.com/zhihanyue/ts2vec
 
 """
 
