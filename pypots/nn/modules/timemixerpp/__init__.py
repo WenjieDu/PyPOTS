0502a3875034a09149e7f2b222288f64cb2d26ac
Wenjie Du <wenjay.du@gmail.com>
2025-03-28 00:51:05 +0800

    feat: add TimeMixer++ modules;

---
+++ pypots/nn/modules/timemixerpp/__init__.py
@@ -0,0 +1,23 @@
+"""
+The package including the modules of TimeMixer++.
+
+Refer to the paper
+`Shiyu Wang, Jiawei Li, Xiaoming Shi, Zhou Ye, Baichuan Mo, Wenze Lin, Ju Shengtong, Zhixuan Chu, Ming Jin.
+"TimeMixer++: A General Time Series Pattern Machine for Universal Predictive Analysis".
+ICLR 2025.
+<https://openreview.net/pdf?id=1CLzLXSFNn>`_
+
+Notes
+-----
+This implementation is inspired by the official one https://anonymous.4open.science/r/TimeMixerPP
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+from .backbone import BackboneTimeMixerPP
+
+__all__ = [
+    "BackboneTimeMixerPP",
+]
