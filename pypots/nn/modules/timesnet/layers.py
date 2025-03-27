8c318167aacbf6a6160df86b8b075b5ed8b832ed
Wenjie Du <wenjay.du@gmail.com>
2025-03-28 00:23:12 +0800

    feat: gather Inception modules;

--- pypots/nn/modules/timesnet/layers.py
+++ pypots/nn/modules/timesnet/layers.py
@@ -10,6 +10,8 @@ import torch.fft
 import torch.nn as nn
 import torch.nn.functional as F
 
+from ..inception import InceptionBlockV1
+
 
 def FFT_for_Period(x, k=2):
     # [B, T, C]
@@ -21,34 +23,6 @@ def FFT_for_Period(x, k=2):
     top_list = top_list.detach().cpu().numpy()
     period = x.shape[1] // top_list
     return period, abs(xf).mean(-1)[:, top_list]
-
-
-class InceptionBlockV1(nn.Module):
-    def __init__(self, in_channels, out_channels, num_kernels=6, init_weight=True):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.num_kernels = num_kernels
-        kernels = []
-        for i in range(self.num_kernels):
-            kernels.append(nn.Conv2d(in_channels, out_channels, kernel_size=2 * i + 1, padding=i))
-        self.kernels = nn.ModuleList(kernels)
-        if init_weight:
-            self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        res_list = []
-        for i in range(self.num_kernels):
-            res_list.append(self.kernels[i](x))
-        res = torch.stack(res_list, dim=-1).mean(-1)
-        return res
 
 
 class TimesBlock(nn.Module):
